348395fbcb368b1d4da97362bf27cb9e3c66e9bf
Ishika Soni <68482663+issoni@users.noreply.github.com>
2021-05-06 15:20:40 -0400

    removed redundancies

--- EmptyListException.java
+++ EmptyListException.java
@@ -3,12 +3,6 @@ package sample;
 /**
  * train car manager for a commercial train
  * @author Ishika Soni
- * @e-mail: ishika.soni@stonybrook.edu
- * @authorID: Stony Brook ID: 113492059
- * @HW#: 2
- * @course: CSE 214
- * @recitation: R02
- * @TA: William Simunek
  */
 
 public class EmptyListException extends Exception {
